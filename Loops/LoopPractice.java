e17688c69191d1d2c746b1b38de427746257086d
shivam3009 <shivamrajsingh3009@gmail.com>
2022-02-10 17:49:53 +0530

    loops

---
+++ Loops/LoopPractice.java
@@ -0,0 +1,15 @@
+public class LoopPractice {
+    public static void main(String[] args) {
+        int i =1;
+
+        while( i < 100){
+            System.out.println(i);
+            i *= 2;
+        }
+
+        do{
+            System.out.println(i);
+            i = i*2;
+        }while(i < 100);
+    }    
+}
