406c86182fdc2c39391198c5376050c313e9056e
shivam3009 <shivamrajsingh3009@gmail.com>
2022-02-14 00:38:20 +0530

    Pattern 1

---
+++ Loops/Pattern1.java
@@ -0,0 +1,12 @@
+public class Pattern1 {
+    public static void main(String[] args) {
+        int count =0;
+        for(int i =0; i < 5; i++){
+            for(int j = 0; j < 5; j++){
+                count++;
+                System.out.format("%02d ", count);
+            }
+            System.out.println("");
+        }
+    }
+}
