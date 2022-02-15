814101f8448fc2232de76a974c5d34f7606c821a
shivam3009 <shivamrajsingh3009@gmail.com>
2022-02-15 15:26:08 +0530

    update value in array

---
+++ Methods/updateValue.java
@@ -0,0 +1,15 @@
+public class updateValue {
+    static void updateValue(int arr[], int index, int newValue){
+        arr[index] = newValue;
+    }
+    
+    public static void main(String[] args) {
+        int arr[] = {2,7,4,5,6};
+        
+        updateValue(arr,1,3);
+
+        for(int x : arr){
+            System.out.print(x + " ");
+        }
+    }    
+}
