584ae84e964e69bf62fb3f73e5e3c86f861ae178
shivam3009 <shivamrajsingh3009@gmail.com>
2022-02-28 17:34:29 +0530

    Implementing Comparable

--- Collections/TreeSetMain.java
+++ Collections/TreeSetMain.java
@@ -1,10 +1,39 @@
 import java.util.*;
 
+class Point implements Comparable{
+    int x,y;
+    Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public String toString(){
+        return "x = " + x + " y = " + y;
+    }
+    @Override
+    public int compareTo(Object o) {
+        Point p = (Point) o;
+        if(x < p.x){
+            return -1;
+        }else if (x > p.x){
+            return 1;
+        }else{
+            if(y < p.y){
+                return -1;
+            }else if (y > p.y){
+                return 1;
+            }else{
+                return 0;
+            }
+        }
+    }
+}
 public class TreeSetMain {
     public static void main(String[] args) {
-        TreeSet <Integer> ts = new TreeSet<>(List.of(10,30,50,60,10,40));
-        ts.add(25);
+        TreeSet <Point> ts = new TreeSet<>();
+        ts.add(new Point(1,3));
+        ts.add(new Point(3,5));
+        ts.add(new Point(3,4));
+        ts.add(new Point(2,3));
         System.out.println(ts);
-        System.out.println(ts.ceiling(52));
     }
 }
