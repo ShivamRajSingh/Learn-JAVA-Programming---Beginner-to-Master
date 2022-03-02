e4aa91a1a8b2681548e0f8e04a426d8e8d06df34
shivam3009 <shivamrajsingh3009@gmail.com>
2022-03-02 13:02:03 +0530

    Hash Map

---
+++ Collections/HashMapMain.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class HashMapMain{
+    public static void main(String[] args) {
+        HashMap <Integer, String> hm = new HashMap<>();
+        hm.put(3, "C");
+        hm.put(4, "D");
+
+        System.out.println(hm);
+
+    }
+}
