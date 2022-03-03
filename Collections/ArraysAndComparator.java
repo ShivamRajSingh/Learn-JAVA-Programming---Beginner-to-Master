5ef9cf9c1a5080983533a90d7ddde34ea65c7cb6
shivam3009 <shivamrajsingh3009@gmail.com>
2022-03-03 22:36:48 +0530

    Implemented Comparator

--- Collections/ArraysAndComparator.java
+++ Collections/ArraysAndComparator.java
@@ -1,17 +1,29 @@
 import java.util.*;
 
+class MyComparator implements Comparator<Integer>{
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        if(o1 == o2){
+            return 0;
+        }else if(o1 < o2){
+            return 1;
+        }else{
+            return -1;
+        }
+    }
+}
 public class ArraysAndComparator {
     public static void main(String[] args) {
-        int [] arr1 = {2,4,6,8,1,3,5};
-        int [] arr2 = {2,4,6,8,1,3,5,7};
+        Integer [] arr1 = {2,4,6,8,1,3,5};
+        Integer [] arr2 = {2,4,6,8,1,3,5,7};
 
         System.out.println(Arrays.compare(arr1,arr2));
 
-        int [] arr3 = Arrays.copyOf(arr1, arr1.length);
+        Integer [] arr3 = Arrays.copyOf(arr1, arr1.length);
 
         // Arrays.fill(arr3, 2);
 
-        Arrays.sort(arr3);
+        Arrays.sort(arr3, new MyComparator());
 
         for(int x : arr3){
             System.out.println(x);
