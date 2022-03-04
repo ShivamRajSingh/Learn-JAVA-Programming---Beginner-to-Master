e50fbffd8f9ad5a2e0e51d0dcb0b55d6b07eed94
shivam3009 <shivamrajsingh3009@gmail.com>
2022-03-04 12:15:44 +0530

    Method reference in lambda expressions

--- Lambda Expressions/LambdaExpressionsMain.java
+++ Lambda Expressions/LambdaExpressionsMain.java
@@ -1,11 +1,33 @@
 @FunctionalInterface
-interface MyLambda{
+interface MyLambda1{
     public int product(int x, int y);
 }
 
+@FunctionalInterface
+interface MyLambda2{
+    public void display(String str);
+}
+
 public class LambdaExpressionsMain {
+    public static void displayReverse(String str){
+        StringBuffer stb = new StringBuffer(str);
+        stb.reverse();
+        System.out.println(stb);
+    }
     public static void main(String[] args) {
-        MyLambda ml = (a,b) -> a*b;
-        System.out.println(ml.product(25, 65));
+        MyLambda1 m = (a,b) -> a*b;
+        System.out.println(m.product(25, 65));
+
+        // Method Reference
+
+        MyLambda2 ml1 = System.out::println;
+
+        MyLambda2 ml2 = LambdaExpressionsMain :: displayReverse; // Method reference using class name for static methods.
+
+        // LambdaExpressionsMain ld = new LambdaExpressionsMain();
+
+        // MyLambda2 ml3 = ld :: displayReverse;
+        
+        ml3.display("Hello");
     }
 }
